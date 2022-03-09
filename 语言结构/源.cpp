680e99655ce8e773cbfac3262ac53710fdfdc8eb
hefayi <3157285958@qq.com>
2022-03-09 22:03:25 +0800

    语言结构

---
+++ 语言结构/源.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+//int main()
+//{
+//	int age = 25;
+//	if (age >78)
+//		printf("未成年\n");
+//	else
+//		//if (18 <= age <= 35)//不能这么用，因为11<18为假，假为0，则变成0<=35然后就输出青年,应用以下代码
+//		/*if(age>=18 && age<=35)
+//			printf("青年\n");*/
+//	/*else
+//		printf("成年\n");*/
+//	return 0;
+//int main()
+//{
+//	int age = 10;
+//	if (age < 18)//当if语句控制多个语句是要用大括号括起来，一对大括号就是一个代码块
+//	{
+//		printf("未成年\n");
+//		printf("不能谈恋爱\n");
+//	}
+//	else
+//	{
+//		printf("成年\n");
+//		printf("可以谈恋爱");
+//	}
+//	return 0;
+//}
+//int main() 
+//{
+//	//int a = 0;
+//	//int b;
+//	//if (a == 1)
+//	//	if (b == 2)
+//	//	else
+//	//		printf("haha\n");//错误写法，对应错误，这个else对应第二个if
+//	return 0;
+//}
+int main()
+{
+	int i = 1;
+	/*while (i <= 100)
+	{
+		if (i % 2 == 1)
+			printf("%d   ", i);
+		i++;
+
+	}*/
+	/*while (i < 10000)
+	{
+		printf("%d  ", i);
+		i += 2;
+	}
+
+	return 0;
+}*/
+
+	
