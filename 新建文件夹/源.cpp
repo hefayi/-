a4a80244c6f02f05fe7619b11f7f380194ab493f
hefayi <3157285958@qq.com>
2022-03-17 18:03:04 +0800

    2022.3.17

---
+++ 新建文件夹/源.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+//int main()
+//{
+//
+//	//while ((ch=getchar())!=EOF)//EOF=end of file文件结束标志
+//	//{
+//	//	putchar(ch);
+//	//}
+//	int ret = 0;
+//	int ch = 0;
+//
+//	char password[20] = { 0 };
+//	printf("请输入密码：>");
+//	scanf("%s", password);//输入密码，并存放在password数组中
+//	//缓冲区还还剩一个空格\N
+//	//读取一下'\n'
+//	while ((ch = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	printf("请确认（Y/N):>");
+//	//getchar();
+//	
+//	ret = getchar();
+//	if (ret == 'Y')
+//	{
+//		printf("确认成功\n");
+//
+//	}
+//	else
+//	{
+//		printf("放弃确认\n");
+//	}
+//	return 0;
+//}
